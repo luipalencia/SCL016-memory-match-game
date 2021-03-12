855f753d4d992a904e16cf8a4b4ed297b4bbb47d
Rodolfo Montecino <vsuazo.93@gmail.com>
2021-03-12 13:15:54 -0300

    cambios

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,10 @@
+//
+// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
+// JavasSript que contenga el `export` correspondiente...
+//
+import pokemon from '../data/pokemon/pokemon.js';
+console.log(pokemon);
+// //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
 //
@@ -10,8 +17,8 @@ const App = () => {
   const el = document.createElement('div');
 
   el.className = 'App';
-  el.innerHTML = '<img src="./resources/pokemon.png" alt="Logo Pokemon" id="logo"><h1>Memorice</h1><h2>¿Estás listo para ser un entrenador Pokemón?</h2><h3>Selecciona con cuántos pares quieres jugar</h3><div id="difficulty"><img src="./resources/pokeball.png" alt="Tres pares de cartas" id="easy"><img src="./resources/superball.png" alt="Seis pares de cartas" id="medium"><img src="./resources/superball1.png" alt="Nueve pares de cartas" id="hard"></div>';
-  
+  el.innerHTML = '<img src="./resources/pokemon.png" alt="Logo Pokemon" id="logo"><h1>Memorice</h1><h2>¿Estás listo para ser un entrenador Pokemón?</h2><h3>Selecciona con cuántos pares quieres jugar</h3><div id="difficulty"><img src="./resources/pokeball.png" alt="Tres pares de cartas" id="easy"><img src="./resources/superball.png" alt="Seis pares de cartas" id="medium"><img src="./resources/ultraball.png" alt="Nueve pares de cartas" id="hard"></div>';
+
   return el;
 };
 
@@ -53,4 +60,3 @@ const congratsPage = () => {
 
 
 export { App, gamePageEasy, gamePageMedium, gamePageHard, congratsPage };
-
