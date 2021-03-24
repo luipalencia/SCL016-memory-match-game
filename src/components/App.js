7a0aba68034120d71466a38ded614843b7c22a0c
luipalencia <luipalencia>
2021-03-24 08:31:05 -0300

    agregar funcion de flipCards, disableCards y unflipCards

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,127 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
 import pokemon from '../data/pokemon/pokemon.js';
-console.log(pokemon);
-// //
-// O alternativamente podríamos cargar el JSON de forma asíncrona usando
-// `fetch` en el momento que consideremos necesario.
-//
-//  fetch('./data/pokemon/pokemon.json')
-//    .then(resp => resp.json())
-//    .then(console.log)
-//    .catch(console.error);
 
-const App = () => {
+/* const App = () => {
   const el = document.createElement('div');
 
   el.className = 'App';
-  el.innerHTML = '<img src="./resources/pokemon.png" alt="Logo Pokemon" id="logo"><h1>Memorice</h1><h2>¿Estás listo para ser un entrenador Pokemón?</h2><h3>Selecciona con cuántos pares quieres jugar</h3><div id="difficulty"><img src="./resources/pokeball.png" alt="Tres pares de cartas" id="easy"><img src="./resources/superball.png" alt="Seis pares de cartas" id="medium"><img src="./resources/ultraball.png" alt="Nueve pares de cartas" id="hard"></div>';
+  el.innerHTML = '<img src="./resources/pokemon.png" alt="Logo Pokemon" id="logo"><h1>Memorice</h1><h2>¿Estás listo para ser un entrenador Pokemón?</h2><h3>Selecciona con cuántos pares quieres jugar</h3><div class="difficulty"><img src="./resources/pokeball.png" alt="Tres pares de cartas" class="dificultad" id="easy"><img src="./resources/superball.png" alt="Seis pares de cartas"  class="dificultad" id="medium"><img src="./resources/ultraball.png" alt="Nueve pares de cartas"  class="dificultad" id="hard"></div><div class=""><img src="./resources/ash.gif" alt="" id="img-footer"></div>';
 
   return el;
+}; */
+
+const gamePage = () => {
+
+/*   let node1 = document.querySelector('#pokemonCards');
+  let text = document.createElement("h1");
+  text.textContent = "¡Voltea las cartas para encontrar pares de tus Pokemones favoritos!";
+  node1.appendChild(text);
+ */
+/*   let playAgain = document.getElementById ('toInit')
+  playAgain.addEventListener('click', () =>{
+    location.reload();
+  }); */
+ 
+
+  function shuffleArray(array) {
+      array.map(function (element, i) {
+          const j = Math.floor(Math.random() * (i + 1));
+          let temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      })
+      return array;
+  }
+  
+      function generarPokemon(pokemons) {
+      pokemons.map(function (element, index) {
+          let node = document.querySelector('#pokemonCards');
+          let divs = document.createElement("div");
+          divs.classList.add("memory-card");
+          divs.classList.add(element.id);
+          node.appendChild(divs);
+          let imagen2 = document.createElement("img");
+          imagen2.className = "card-backside";
+          imagen2.src = element.image;
+          imagen2.style.background = element.bgColor;
+          let imagen = document.createElement("img");
+          imagen.className = "card-upside";
+          imagen.src = "./resources/backside.png";
+          divs.appendChild(imagen2);
+          divs.appendChild(imagen);
+      }) 
+
+  }
+  generarPokemon(shuffleArray(pokemon.items));
+  generarPokemon(shuffleArray(pokemon.items));
+
+  const cards = document.querySelectorAll('.memory-card');
+
+  let hasFlippedCard = false; 
+  let lockBoard = false;
+  let firstCard, secondCard;  
+
+
+  function flipCard(e) {
+    if (lockBoard) return;
+    if (e.target.parentNode === firstCard) return;
+
+    e.target.parentNode.classList.add('flip');
+
+    
+    if(!hasFlippedCard){
+      hasFlippedCard = true;
+      firstCard = e.target.parentNode;
+
+      return;
+    } 
+      secondCard = e.target.parentNode;
+
+      checkForMatch();
+  }
+
+  function checkForMatch() {
+    let isMatch = firstCard.className === secondCard.className; 
+     
+    isMatch ? disableCards() : unflipCards();
+
+  }
+
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+  }
+
+  function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
+      resetBoard();
+    }, 1500);
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+   setTimeout(function() {
+    cards.forEach(card => addEventListener('click', flipCard))
+   }, 500); 
+
 };
 
-const gamePageEasy = () => {
-  const el = document.createElement('div');
-
-  el.className = 'gamePageEasy';
-  el.innerHTML =  '';
-
-  return el;
-};
-
-const gamePageMedium = () => {
-  const el = document.createElement('div');
-
-  el.className = 'gamePageMedium';
-  el.innerHTML =  '';
-
-  return el;
-};
-
-const gamePageHard = () => {
-  const el = document.createElement('div');
-
-  el.className = 'gamePageHard';
-  el.innerHTML =  '';
-
-  return el;
-};
 const congratsPage = () => {
   const el = document.createElement('div');
 
   el.className = 'congratsPage';
-  el.innerHTML =  '<h4>Felicitaciones</h4><img src="./resources/celebrate.gif" alt="Pokemones celebrando victoria" id="celebrate1"><img src="./resources/confetti.gif" alt="Confetti" id="celebrate2">';
+  el.innerHTML = '<h4>Felicitaciones</h4><img src="./resources/celebrate.gif" alt="Pokemones celebrando victoria" id="celebrate1"><img src="./resources/confetti.gif" alt="Confetti" id="celebrate2">';
 
   return el;
 };
 
-
-
-export { App, gamePageEasy, gamePageMedium, gamePageHard, congratsPage };
+export { gamePage, congratsPage };
--- src/main.js
+++ src/main.js
@@ -1,48 +1,27 @@
-import { App, gamePageEasy, gamePageMedium, gamePageHard, congratsPage } from './components/App.js';
-import pokemon from '../data/pokemon/pokemon.js';
+import { gamePage, congratsPage } from './components/App.js';
 
 
-document.getElementById('root').appendChild(App());
 
-let buttonToEasy = document.querySelector('#easy');
-buttonToEasy.addEventListener("click", function () {
-
-    document.querySelector('#root').style.display = "none";
-    function shuffleArray(array) {
-        array.map(function (element, i) {
-            const j = Math.floor(Math.random() * (i + 1));
-            let temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        })
-        return array;
+ let dificultades = document.getElementsByClassName('dificultad');
+for (let i = 0; i < dificultades.length; i++) {
+  dificultades[i].addEventListener('click', (event) => {
+    switch (event.target.id) {
+      case "easy":
+        document.querySelector('#root').style.display = "none";
+        document.querySelector('#gamePage').style.display = "block";
+        gamePage();
+        break;
+      case "medium":
+        document.querySelector('#root').style.display = "none";
+        document.querySelector('#gamePage').style.display = "block";
+        gamePage();
+        break;
+      case "hard":
+      default:
+        document.querySelector('#root').style.display = "none";
+        document.querySelector('#congratsPage').style.display = "block";
+        gamePage();
+        break;
     }
-
-    function generarPokemon(pokemons) {
-        pokemons.map(function (element, index) {
-            let node = document.querySelector('#pokemonCards');
-            let imagen = document.createElement("img");
-            imagen.className = "card-upside";
-            imagen.style.background = element.bgColor;
-            imagen.src = element.image;
-            node.appendChild(imagen);
-        })
-    }
-    generarPokemon(shuffleArray(pokemon.items));
-    generarPokemon(shuffleArray(pokemon.items));
-});
-
-let buttonToMedium = document.querySelector('#medium');
-buttonToMedium.addEventListener("click", function () {
-
-    document.querySelector('#root').style.display = "none";
-    document.querySelector('#gamePageMedium').appendChild(gamePageMedium());
-
-});
-
-let buttonToHard = document.querySelector('#hard');
-buttonToHard.addEventListener("click", function () {
-
-    document.querySelector('#root').style.display = "none";
-
-});
+  })
+} 
