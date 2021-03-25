ed799f2cafd216c6906fa8335f9754c158c400e4
luipalencia <luipalencia>
2021-03-25 08:24:38 -0300

    cambios sobre funcion checkForMatch

--- src/components/App.js
+++ src/components/App.js
@@ -11,44 +11,43 @@ import pokemon from '../data/pokemon/pokemon.js';
 
 const gamePage = () => {
 
-/*   let node1 = document.querySelector('#pokemonCards');
+  let node1 = document.querySelector('#pokemonCards');
   let text = document.createElement("h1");
   text.textContent = "¡Voltea las cartas para encontrar pares de tus Pokemones favoritos!";
   node1.appendChild(text);
- */
-/*   let playAgain = document.getElementById ('toInit')
-  playAgain.addEventListener('click', () =>{
-    location.reload();
-  }); */
- 
+
+  /*  let playAgain = document.getElementById('toInit');
+   playAgain.addEventListener('click', () =>{
+     location.reload();
+   }); */
 
   function shuffleArray(array) {
-      array.map(function (element, i) {
-          const j = Math.floor(Math.random() * (i + 1));
-          let temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-      })
-      return array;
+    array.map(function (element, i) {
+      const j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    })
+    return array;
   }
-  
-      function generarPokemon(pokemons) {
-      pokemons.map(function (element, index) {
-          let node = document.querySelector('#pokemonCards');
-          let divs = document.createElement("div");
-          divs.classList.add("memory-card");
-          divs.classList.add(element.id);
-          node.appendChild(divs);
-          let imagen2 = document.createElement("img");
-          imagen2.className = "card-backside";
-          imagen2.src = element.image;
-          imagen2.style.background = element.bgColor;
-          let imagen = document.createElement("img");
-          imagen.className = "card-upside";
-          imagen.src = "./resources/backside.png";
-          divs.appendChild(imagen2);
-          divs.appendChild(imagen);
-      }) 
+
+  function generarPokemon(pokemons) {
+    pokemons.map(function (element, index) {
+      let node = document.querySelector('#pokemonCards');
+      let divs = document.createElement("div");
+      divs.classList.add("memory-card");
+      divs.classList.add(element.id);
+      node.appendChild(divs);
+      let imagen2 = document.createElement("img");
+      imagen2.className = "card-backside";
+      imagen2.src = element.image;
+      imagen2.style.background = element.bgColor;
+      let imagen = document.createElement("img");
+      imagen.className = "card-upside";
+      imagen.src = "./resources/backside.png";
+      divs.appendChild(imagen2);
+      divs.appendChild(imagen);
+    })
 
   }
   generarPokemon(shuffleArray(pokemon.items));
@@ -56,9 +55,9 @@ const gamePage = () => {
 
   const cards = document.querySelectorAll('.memory-card');
 
-  let hasFlippedCard = false; 
+  let hasFlippedCard = false;
   let lockBoard = false;
-  let firstCard, secondCard;  
+  let firstCard, secondCard;
 
 
   function flipCard(e) {
@@ -67,23 +66,34 @@ const gamePage = () => {
 
     e.target.parentNode.classList.add('flip');
 
-    
-    if(!hasFlippedCard){
+
+    if (!hasFlippedCard) {
       hasFlippedCard = true;
       firstCard = e.target.parentNode;
 
       return;
-    } 
-      secondCard = e.target.parentNode;
+    }
+    secondCard = e.target.parentNode;
 
-      checkForMatch();
+    checkForMatch();
   }
-
+  let correct = 0;
   function checkForMatch() {
-    let isMatch = firstCard.className === secondCard.className; 
-     
-    isMatch ? disableCards() : unflipCards();
+    let isMatch = firstCard.className === secondCard.className;
 
+    isMatch ? disableCards() : unflipCards();
+    if (isMatch === true) {
+      correct += 1;
+    }
+    if (correct === 2) {
+      document.querySelector('#gamePage').style.display = "none";
+      document.querySelector('#congratsPage').style.display = "block";
+      var el = document.querySelector('#congratsPage');
+
+      el.innerHTML = '<img src="./resources/congrats.png" alt="" id="celebrate3"><img src="./resources/celebrate.gif" alt="Pokemones celebrando victoria" id="celebrate1"><button id="toInit">Inicio</button>';
+
+      return el;
+    }
   }
 
   function disableCards() {
@@ -109,17 +119,22 @@ const gamePage = () => {
     [firstCard, secondCard] = [null, null];
   }
 
-   setTimeout(function() {
+  setTimeout(function () {
     cards.forEach(card => addEventListener('click', flipCard))
-   }, 500); 
-
+  }, 500);
 };
+
 
 const congratsPage = () => {
   const el = document.createElement('div');
 
   el.className = 'congratsPage';
-  el.innerHTML = '<h4>Felicitaciones</h4><img src="./resources/celebrate.gif" alt="Pokemones celebrando victoria" id="celebrate1"><img src="./resources/confetti.gif" alt="Confetti" id="celebrate2">';
+  el.innerHTML = '<img src="./resources/congrats.png" alt="" id="celebrate3"><img src="./resources/celebrate.gif" alt="Pokemones celebrando victoria" id="celebrate1"><button id="toInit">Inicio</button>';
+
+  let playAgain = document.getElementById('toInit');
+  playAgain.addEventListener('click', () => {
+    location.reload();
+  });
 
   return el;
 };
